01aa94bf42122aeca1d9bace0d0a65334019e305
kin1826 <tvbphu2@gmail.com>
2026-05-11 16:03:22 +0700

    fect(base): create objective market

---
+++ Assets/Scripts/Base/ObjectiveTrigger.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ObjectiveTrigger : MonoBehaviour
+{
+    public ObjectiveMarkerSystem objectiveSystem;
+
+    public bool nextObjective = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        // 🚀 objective tiếp theo
+        if (nextObjective)
+        {
+            objectiveSystem.NextObjective();
+        }
+
+        // 🚀 hoặc tắt objective
+        else
+        {
+            objectiveSystem.EndObjective();
+        }
+
+        // 🚀 tránh trigger lại
+        gameObject.SetActive(false);
+    }
+}
